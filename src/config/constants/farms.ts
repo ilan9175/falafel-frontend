145c0404749b22cb5e7f5683927dffefdd47d620
Ilan <ibinisti@gmail.com>
2021-04-08 18:57:19 +0200

    temp change

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -9,7 +9,7 @@ const farms: FarmConfig[] = [
       97: '',
       56: '0xE9C53B5Ab0C9cDBd72A03151a628863C28c55A6A',
     },
-    tokenSymbol: 'FLF',
+    tokenSymbol: 'BLZD',
     tokenAddresses: {
       97: '',
       56: '0x57067A6BD75c0E95a6A5f158455926e43E79BeB0',
