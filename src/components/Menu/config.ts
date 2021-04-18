630cbcf1f035091cc74297c6d4509534d6a6b719
Ilan <ibinisti@gmail.com>
2021-04-18 18:26:17 +0200

    comment blizzard pool

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -32,11 +32,11 @@ const config: MenuEntry[] = [
     icon: 'CaveIcon',
     href: '/caves',
   },
-  {
+  /*{
     label: 'Blizzard',
     icon: 'PoolIcon',
     href: '/pools',
-  },
+  },*/
   {
     label: 'Info',
     icon: 'InfoIcon',
